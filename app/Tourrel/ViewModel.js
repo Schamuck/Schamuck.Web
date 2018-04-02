412fe19660f853d1e725c30fa45b30c9d911d43e
rokon <rokon.m.jaman@gmail.com>
2018-04-02 22:09:30 +0200

    tried cost onChange event

--- app/Tourrel/ViewModel.js
+++ app/Tourrel/ViewModel.js
@@ -1,37 +1,59 @@
 window.onload = function () {
     let vm = new TourrelViewModel();
-    vm.cal = function () {
-        alert("sfjlsj");
-    };
     ko.applyBindings(vm);
 };
-function person(name, amount) {
+
+function person(name, amount) 
+{
     var self = this;
-    self.name = name;
-    self.contribution = ko.observable(amount);
-    self.contribution.subscribe(function () {
-        console.log("event fired");
-    });
+    this.name = name;
+    this.contribution = ko.observable(amount);
+    this.calc = function()
+    {
+       //vm.totalCost.bind(this); 
+    }
+    // self.contribution.subscribe(function () 
+    // {
+    //     console.log("event fired");
+    // });
 }
-function TourrelViewModel() {
+
+function TourrelViewModel() 
+{
     var self = this;
     self.expenses = [33, 56];
     self.personInstances = ko.observableArray([
         new person("person1", self.expenses[0]),
         new person("person2", self.expenses[1])
     ]);
-    self.totalCost = ko.computed(function () {
-        var total = 0;
-        for (var i = 0; i < self.personInstances().length; i++)
-            total += self.personInstances()[i].contribution();
-        return total;
+    // self.totalCost = ko.computed(function () {
+    //     var total = 0;
+    //     for (var i = 0; i < self.personInstances().length; i++)
+    //         total += self.personInstances()[i].contribution();
+    //     return total;
+    // });
+
+    self.totalCost = ko.pureComputed({
+        read: function () {
+            var total = 0;
+            for (var i = 0; i < self.personInstances().length; i++)
+                total += self.personInstances()[i].contribution();
+            return total ;
+        },
+        write: function (value) {
+            return (self.totalCost +value);
+        },
+        owner: this
     });
+
     self.headAverage = ko.computed(function () {
         return self.totalCost() / self.personInstances().length;
-        ;
+        
     });
-    self.addPerson = function () {
+    self.addPerson = function () 
+    {
         self.personInstances.push(new person("", 0));
     };
-    self.removePerson = function (seat) { self.personInstances.remove(seat); };
+    self.removePerson = function (person) { self.personInstances.remove(person); };
+
 }
